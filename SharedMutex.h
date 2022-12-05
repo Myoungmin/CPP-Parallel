9a9437816752989afd32dd807e4625d0f99d3ced
Myoungmin <miung222@gmail.com>
2022-12-05 21:54:20 +0900

    shared_mutex RAII 적용

--- SharedMutex.h
+++ SharedMutex.h
@@ -13,19 +13,27 @@ struct MutexInt
 void SetNum(MutexInt& mi, int num)
 {
 	// 쓰기 동작이기 때문에 Exclusive Lock을 걸어준다.
-	
-	mi.mtx.lock();
+
+	/*mi.mtx.lock();
 	mi.num = num;
-	mi.mtx.unlock();
+	mi.mtx.unlock();*/
+
+	// RAII 적용
+	std::lock_guard<std::shared_mutex> lck(mi.mtx);
+	mi.num = num;
 }
 
 void PrintNum(MutexInt& mi)
 {
 	// 읽기 동작이기 때문에 Shared Lock을 걸어준다.
-	
-	mi.mtx.lock_shared();
+
+	/*mi.mtx.lock_shared();
 	std::cout << "num: " << mi.num << std::endl;
-	mi.mtx.unlock_shared();
+	mi.mtx.unlock_shared();*/
+
+	// RAII 적용
+	std::shared_lock<std::shared_mutex> lck(mi.mtx);
+	std::cout << "num: " << mi.num << std::endl;
 }
 
 
